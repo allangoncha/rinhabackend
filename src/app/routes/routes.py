220f40afd4c841b700b5586254220840f9cdc0da
allangoncha <allan.goncharenco@hotmail.com>
2023-09-08 22:30:09 -0300

    Load Envs, create db connection, first models and endpoint pessoas

--- src/app/routes/routes.py
+++ src/app/routes/routes.py
@@ -3,6 +3,29 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 import psycopg2, os
 
-@app.get("/")
-def hello():
-    return {"Message": "Hello, World!"}
+#Load Env's
+load_dotenv()
+HOST = os.environ.get("host")
+PORT = os.environ.get("port")
+USER = os.environ.get("user")
+PASS = os.environ.get("pass")
+DBNAME = os.environ.get("dbname")
+
+#DB Connection
+conn = psycopg2.connect(host=HOST, port=PORT, user=USER, password=PASS, dbname=DBNAME)
+cur = conn.cursor()
+
+#Models
+class Pessoas(BaseModel):
+    apelido: str
+    nome: str 
+    nascimento: str #AAAA-MM-DD
+    stack: list
+
+@app.post("/pessoas", response_model=Pessoas)
+def pessoas(pessoas: Pessoas):
+    return {"apelido": pessoas.apelido,
+            "nome": pessoas.nome,
+            "nascimento": pessoas.nascimento,
+            "stack": pessoas.stack
+            }
