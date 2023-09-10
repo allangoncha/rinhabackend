143e4981fba7ba6523a3b399629f69bba0356512
allangoncha <allan.goncharenco@hotmail.com>
2023-09-10 19:44:48 -0300

    Create first endpoint

--- src/app/routes/routes.py
+++ src/app/routes/routes.py
@@ -1,8 +1,11 @@
 from src.app import app
+from fastapi import Response
+from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from typing import List, Optional
-import psycopg2, os
+from sqlalchemy import insert, create_engine, text
+import os, uuid
 
 #Load Env's
 load_dotenv()
@@ -13,8 +16,12 @@ PASS = os.environ.get("pass")
 DBNAME = os.environ.get("dbname")
 
 #DB Connection
-conn = psycopg2.connect(host=HOST, port=PORT, user=USER, password=PASS, dbname=DBNAME)
-cur = conn.cursor()
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASS}@{HOST}:{PORT}/{DBNAME}"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, pool_size=10
+)
+
+conn = engine.connect()
 
 #Models
 class Pessoas(BaseModel):
@@ -23,10 +30,30 @@ class Pessoas(BaseModel):
     nascimento: str #AAAA-MM-DD
     stack: Optional[List[str]]
 
-@app.post("/pessoas", response_model=Pessoas)
-def pessoas(pessoas: Pessoas):
-    return {"apelido": pessoas.apelido,
-            "nome": pessoas.nome,
-            "nascimento": pessoas.nascimento,
-            "stack": pessoas.stack
-            }
+@app.post("/pessoas", response_model=Pessoas, status_code = 201)
+def pessoas(pessoas: Pessoas, response: Response):
+    id_pessoas = uuid.uuid4()
+
+    if pessoas.stack != None:
+        stack_array = "{" + ",".join(pessoas.stack) + "}"
+    else:
+        stack_array = 'null'
+
+    try:
+        insert_pessoas = f"""
+        INSERT INTO public.pessoas
+        (id, apelido, nome, nascimento, stack)
+        VALUES('{id_pessoas}', '{pessoas.apelido}', '{pessoas.nome}', '{pessoas.nascimento}', '{stack_array}');
+        """
+        conn.execute(text(insert_pessoas))
+        conn.commit()
+
+        headers = {"Location": f'pessoas/{id_pessoas}'}
+        content = "Hello, world!"
+        
+        return JSONResponse(content=content, headers=headers)
+
+    except Exception as e:
+        return {
+                "Exception": {e}
+                }
